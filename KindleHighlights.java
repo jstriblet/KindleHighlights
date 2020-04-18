fab37343840952c66446bc379fac9f4ffb1ba270
Jonathan Striblet <jstriblet@gmail.com>
2020-04-17 18:05:30 -0700

    reading file size

--- KindleHighlights.java
+++ KindleHighlights.java
@@ -1,13 +1,35 @@
 import java.io.File; 
 import java.util.Scanner; 
 
+class highlightRecord {
+    String title;
+    String meta;
+    String text;
+}
+
 public class KindleHighlights {
     public static void main( String[] args) throws Exception 
     {
-        File file = new File("/Users/striblet/Desktop/MyClippings.txt");
-        Scanner sc = new Scanner(file);
+        Scanner inFile = new Scanner(new File("/Users/striblet/Documents/CS/KindleHighlights/My Clippings.txt"));
+        highlightRecord[] kindleDB;
+        int count = 0;
+        String text;
 
-        while (sc.hasNextLine()) 
-            System.out.println(sc.nextLine()); 
+        // Count how many highlights I have
+        while (inFile.hasNextLine()) {
+            text = inFile.nextLine();
+            if ( text.equals("==========" ) ) {
+                count++;
+            }
+        }
+        inFile.close();
+
+        System.out.println( count + " records in file." );
+
+        inFile = new Scanner(new File("/Users/striblet/Documents/CS/KindleHighlights/My Clippings.txt"));
+        inFile.close();
+       
+
+
     }
 }   
--- Note.java
+++
@@ -1,3 +0,0 @@
-public class Note {
-    int x = 5;
-}
