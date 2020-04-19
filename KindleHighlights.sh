bfed15742d0abb6c1ed290b34986a75a82bff177
Jonathan Striblet <jstriblet@gmail.com>
2020-04-18 20:16:35 -0700

    working prototype

--- KindleHighlights.sh
+++ KindleHighlights.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 cd /Users/striblet/Documents/CS/KindleHighlights
 osascript SendEmail.scpt
+
+# Create a LaunchAgent @ ~/Library/LaunchAgent to run this script automatically
+# Copy of the one I used in: com.KindleHighlights.plist
