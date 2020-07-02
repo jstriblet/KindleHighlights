85643db346e389982ec483b914e747b6e6552ebb
Jonathan Striblet <jstriblet@gmail.com>
2020-07-01 20:30:44 -0700

    auto import

---
+++ import.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+cd ~/Documents/CS/KindleHighlights/
+
+NOW=$(date +'%Y%m%d')
+
+if [ -d "/Volumes/Kindle/documents/" ] 
+then
+	cp "My Clippings.txt" "hist/My Clippings.txt-$NOW"
+	cp "/Volumes/Kindle/documents/My Clippings.txt" .
+	echo "Directory Exists $NOW"
+else
+	echo "Directory Does Not Exists"
+fi
