fb6d0609506403088b48f395a2eb773bf7ea5df9
Jonathan Striblet <jstriblet@gmail.com>
2020-07-04 12:38:07 -0700

    .

--- import.sh
+++ import.sh
@@ -5,8 +5,10 @@ NOW=$(date +'%Y%m%d')
 
 if [ -d "/Volumes/Kindle/documents/" ] 
 then
+
 	cp "My Clippings.txt" "hist/My Clippings.txt-$NOW"
 	cp "/Volumes/Kindle/documents/My Clippings.txt" .
+
 	echo "Directory Exists $NOW"
 else
 	echo "Directory Does Not Exists"
